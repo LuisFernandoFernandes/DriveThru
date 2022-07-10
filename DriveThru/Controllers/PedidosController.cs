a27367090636d7675a462ced75d8f63c20db372b
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2022-07-10 13:51:54 -0300

    .

--- DriveThru/Controllers/PedidosController.cs
+++ DriveThru/Controllers/PedidosController.cs
@@ -10,23 +10,18 @@ namespace DriveThru.Controllers
     {
 
         private static List<Pedidos> pedidos = new List<Pedidos>();
+        private static List<Pedidos> fazendo = new List<Pedidos>();
+        private static List<Pedidos> finalizado = new List<Pedidos>();
 
         private static int countDtforBalcao = 0;
         private static int countDtforDelivery = 0;
         private static int countBalcaforDelivery = 0;
 
-        //private static int countPedidoDt = 0;
-        //private static int countPedidoDt = 0;
-        //private static int countPedidoBalcao = 0;
-
         [HttpPost]
         public async Task<ActionResult<Pedidos>> RealizarPedido(eOrigemPedido origemPedido)
         {
-            //a cada dois pedidos do dt um do balcao precisa ser atendido
-
-
             if (countDtforBalcao == 2 && origemPedido != eOrigemPedido.Balcao) return BadRequest();
-            if (countDtforDelivery == 3 || countBalcaforDelivery == 2 && origemPedido != eOrigemPedido.Delivery) return BadRequest();
+            if ((countDtforDelivery == 3 || countBalcaforDelivery == 2) && origemPedido != eOrigemPedido.Delivery) return BadRequest();
             else
             {
                 if (origemPedido == eOrigemPedido.DriveThru)
@@ -44,22 +39,7 @@ namespace DriveThru.Controllers
                     countDtforDelivery = 0;
                     countBalcaforDelivery = 0;
                 }
-
-
-
             }
-
-
-            //if (countPedidoDt == 2 && countPedidoBalcao == 0 && origemPedido != eOrigemPedido.Balcao) return BadRequest();
-            //else if (countPedidoDt == 3 || countPedidoBalcao == 2 && origemPedido != eOrigemPedido.Delivery) return BadRequest();
-
-            //if (origemPedido == eOrigemPedido.DriveThru) countPedidoDt++;
-            //else if (origemPedido == eOrigemPedido.Balcao) countPedidoBalcao++;
-            //else
-            //{
-            //    countPedidoDt = 0;
-            //    countPedidoBalcao = 0;
-            //}
 
             Pedidos pedido = new Pedidos
             {
@@ -72,7 +52,7 @@ namespace DriveThru.Controllers
             return Ok(pedido);
         }
 
-        [HttpPatch]
+        [HttpPatch("{senha}")]
         public async Task<ActionResult<Pedidos>> AlterarPedido(int senha, [FromBody] Pedidos pedido)
         {
             var oldPedido = pedidos.Find(a => a.Senha == senha);
@@ -81,9 +61,81 @@ namespace DriveThru.Controllers
 
             pedidos.Remove(oldPedido);
 
+            pedido.StatusPedido = eStatusPedido.Alterado;
+
             pedidos.Add(pedido);
             return Ok(pedido);
         }
 
+        [HttpPatch("fazer")]
+        public async Task<ActionResult<List<Pedidos>>> FazerPedido()
+        {
+
+            var count = 0;
+            foreach (var pedido in pedidos)
+            {
+
+                if (count == 3) return BadRequest();
+
+                if (pedido.StatusPedido == eStatusPedido.Aguardando || pedido.StatusPedido == eStatusPedido.Alterado)
+                {
+                    pedido.StatusPedido = eStatusPedido.Fazendo;
+                    fazendo.Add(pedido);
+                    return Ok(fazendo);
+                }
+                if (pedido.StatusPedido == eStatusPedido.Fazendo)
+                {
+
+                    fazendo.Add(pedido);
+
+                    count++;
+                }
+
+            }
+            return NotFound();
+        }
+
+        [HttpPatch("finalizar")]
+        public async Task<ActionResult<List<Pedidos>>> FinalizarPedido()
+        {
+            if (fazendo.Count == 0) return NotFound();
+            foreach (var pedido in fazendo)
+            {
+                var pedidosAux = pedidos.Find(a => a.Senha == pedido.Senha);
+                pedidosAux.StatusPedido = eStatusPedido.Finalizado;
+                fazendo.Remove(pedido);
+                finalizado.Add(pedidosAux);
+            }
+            return Ok(finalizado);
+        }
+
+        [HttpPatch("entregar")]
+        public async Task<ActionResult<List<Pedidos>>> EntregaPedido()
+        {
+            var count = 0;
+            List<Pedidos> entregando = new List<Pedidos>();
+            List<Pedidos> finalizadoAux = finalizado;
+            foreach (var pedido in finalizadoAux)
+            {
+                if (pedido.OrigemPedido != eOrigemPedido.Delivery) continue;
+
+                entregando.Add(pedido);
+                finalizadoAux.Remove(pedido);
+                count++;
+
+                if (count == 3)
+                {
+
+                    finalizado = finalizadoAux;
+                    //foreach (var item in collection)
+                    //{
+
+                    //}
+
+                    return Ok(entregando);
+                }
+            }
+            return NotFound();
+        }
     }
 }
--- DriveThru/Enums/eStatusPedido.cs
+++ DriveThru/Enums/eStatusPedido.cs
@@ -6,7 +6,6 @@
         Alterado = 1,
         Fazendo = 2,
         Finalizado = 3,
-        Entregando = 4,
-        Pronto = 5,
+        Pronto = 4,
     }
 }
